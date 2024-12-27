19eb06a3cf9e5dec62dd98074df6075f4c409e2b
Kanchana Buddhika Sugathapala <kanchana2@gmail.com>
2024-12-27 11:36:57 +0530

    changes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const TinyURL = require('tinyurl');
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/', bodyParser.urlencoded({extended:false}));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -17,6 +20,21 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+app.post('/api/shorturl', (req, res, next) => {
+  console.log(req.body);
+  next();
+}, (req, res) =>{
+  let longUrl = req.body.url;
+  console.log(`long url ${longUrl}`);
+  TinyURL.shorten(longUrl)
+    .then((shortened) => {
+        console.log('Shortened URL:', shortened);
+        res.json({"original_url": longUrl, "short_url": shortened});
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 });
 
 app.listen(port, function() {
