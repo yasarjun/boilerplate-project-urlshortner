9ad6cb9707ef2934e9a49c212e1665c474069772
Kanchana Buddhika Sugathapala <kanchana2@gmail.com>
2024-12-29 08:53:34 +0530

    changes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const app = express();
 const TinyURL = require('tinyurl');
 const bodyParser = require('body-parser');
+const dns = require('dns');
+const url = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -21,8 +23,28 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.use('/api/shorturl', (req, res, next) =>{
+  let url_ = (req.body.url == null)? (`${req.protocol}://${req.get('host')}${req.originalUrl}`) : req.body.url;
+  if (url_.indexOf('localhost') < 0){
+    console.log('---------- dns lookup ------');
+    dns.lookup(url_, {all: true}, (err, addresses)=>{
+      if(err){
+        res.json({'error': 'invalid url'})
+      }
+      else{
+        next();
+      }
+    });  
+  }
+  else{
+    next();
+  }
+});
+
+
 app.post('/api/shorturl', (req, res, next) => {
-  console.log(req.body);
+  console.log(req.body);  
   next();
 }, (req, res) =>{
   let longUrl = req.body.url;
@@ -30,11 +52,25 @@ app.post('/api/shorturl', (req, res, next) => {
   TinyURL.shorten(longUrl)
     .then((shortened) => {
         console.log('Shortened URL:', shortened);
-        res.json({"original_url": longUrl, "short_url": shortened});
+        const shortenedUrlObj = url.parse(shortened, true);
+        res.json({"original_url": longUrl, "short_url": shortenedUrlObj.path.substring(1,shortenedUrlObj.path.length)});
     })
     .catch((error) => {
         console.error('Error:', error);
     });
+});
+
+app.get("/api/shorturl/:short_url", (req, res)=>{
+  console.log('--------resolving'+req.params.short_url);
+  let tiny_url = `https://tinyurl.com/${req.params.short_url}`;
+  TinyURL.resolve(tiny_url)
+  .then((original)=> {
+    console.log(`Original url ${original}`);
+    res.redirect(original);
+  })
+  .catch((error)=> {
+    console.error("Error", error);
+  });
 });
 
 app.listen(port, function() {
